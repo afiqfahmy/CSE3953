3f8b261dd1bef3f984fdcde3bb9fff1fcef4564c
root <afiqfahmy3@gmail.com>
2026-04-01 15:16:08 +0800

    Initial Spring Boot Setup

---
+++ src/test/java/com/redox_rx/management_system/ManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redox_rx.management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
