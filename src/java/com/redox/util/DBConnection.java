32d3a72585dce507c4ce92787865cfda3c6651ae
root <afiqfahmy3@gmail.com>
2026-06-20 16:10:36 +0800

    .

--- src/java/com/redox/util/DBConnection.java
+++ src/java/com/redox/util/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/cse3953";
     private static final String USER = "root";
-    private static final String PASS = "";
+    private static final String PASS = "toor";
 
     public static Connection getConnection() throws SQLException {
         try {
